085a8ce63c44d929b0436e78a91432d3e6e20ffb
Julia Koroleva <youlka25@yandex.ru>
2022-10-15 18:11:14 +0300

    add telegram logger

--- config/logging.php
+++ config/logging.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Logging\Telegram\TelegramLoggerFactory;
 use Monolog\Handler\NullHandler;
 use Monolog\Handler\StreamHandler;
 use Monolog\Handler\SyslogUdpHandler;
@@ -117,6 +118,15 @@ return [
         'emergency' => [
             'path' => storage_path('logs/laravel.log'),
         ],
+
+        'telegram' => [
+            'driver' => 'custom',
+            'via' => TelegramLoggerFactory::class,
+            'level' => env('LOG_LEVEL', 'debug'),
+            'chat_id' => -814342025,
+            'token' => '5654495811:AAE5hWfjQfeAn_1aBQ6ImagFzoY5CJtwkLw'
+
+        ]
     ],
 
 ];
