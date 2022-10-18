8b53fdc8abc945868e5a3ae7523e45b2c2889b29
Julia Koroleva <youlka25@yandex.ru>
2022-10-18 20:05:50 +0300

    add brand. category, product

---
+++ app/Models/Product.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+
+class Product extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'thumbnail',
+        'price',
+        'brand_id'
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function (Product $product) {
+            $product->slug = $product->slug ?? str($product->title)->slug();
+        });
+    }
+
+    /**
+     * @return BelongsTo
+     */
+    public function brand()
+    {
+        return $this->belongsTo(Brand::class);
+    }
+
+    /**
+     * @return BelongsToMany
+     */
+    public function categories(): BelongsToMany
+    {
+        return $this->belongsToMany(Category::class);
+    }
+}
